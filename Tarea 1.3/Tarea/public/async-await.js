d4b1f9087f54d584d34ae7a0390513443f967f25
Alejandro Calva <101780546+JuniorCalva40@users.noreply.github.com>
2022-10-25 00:20:35 -0500

    Update async-await.js

--- Tarea 1.3/Tarea/public/async-await.js
+++ Tarea 1.3/Tarea/public/async-await.js
@@ -1,10 +1,5 @@
 
-  function wait(ms = 0) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
-    });
-  }
-
+ 
   async function go() {
     console.log('Hola que tal');
     await wait(2000);
